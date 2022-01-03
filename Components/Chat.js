b0fbe653ba5a6f5a50a3eb93c003927348671f29
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2022-01-03 14:05:44 +0100

    Android keyboard/ system message

--- Components/Chat.js
+++ Components/Chat.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { StyleSheet, View, Text, backgroundColor, Alert } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
+import { Platform, KeyboardAvoidingView } from 'react-native';
+
+const image = require('../Images/profile.jpg');
+
 export default class Chat extends React.Component {
   constructor() {
     super();
@@ -9,16 +13,56 @@ export default class Chat extends React.Component {
     }
   }
 
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true
+        }
+      ],
+    })
+  }
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+  
   render() {
     let username = this.props.route.params.username;
-    let color = this.props.route.params.color;
+    // let color = this.props.route.params.color;
     this.props.navigation.setOptions({ title: username });
     
   return (
-    <View style={[styles.chat, {backgroundColor: color}]}>
-      <Text style={styles.text}>Hello {username}, this is your chat page</Text>
-      <Text></Text>
+    <View style={styles.chatWrapper}>
+    <GiftedChat
+    messages={this.state.messages}
+    onSend={messages => this.onSend(messages)}
+    user={{
+      _id: 1,
+      name: {username},
+      avatar: image
+    }}
+    />
+    { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
+    }
     </View>
+    // <View style={[styles.chat, {backgroundColor: color}]}>
+    //   <Text style={styles.text}>Hello {username}, this is your chat page</Text>
+    // </View>
   )
   }
 }
@@ -34,5 +78,8 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 20,
     marginTop: 50
+  },
+  chatWrapper: {
+    flex: 1,
   }
 });
